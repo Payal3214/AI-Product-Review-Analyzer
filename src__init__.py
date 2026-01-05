afad74aab94114f62ce1ac9598e454c6b7f5bb52
Payal Priyadarshini <80968938+Payal3214@users.noreply.github.com>
2026-01-05 17:04:34 +0530

    Update src directory structure
    Restructure src directory to include necessary files.

--- src__init__.py
+++ src__init__.py
@@ -2,10 +2,9 @@ ai-product-review-analyzer-with-rag-system/
 │
 ├ app.py
 ├ requirements.txt
-├ setup.py        ← this must be a FILE
-├ src/            ← this must be a FOLDER
-│   ├ __init__.py
-│   ├ rag.py
-│   ├ cleaner.py
-│   ├ embeddings.py
-│   └ ...
+├ setup.py                 
+├ __init__.py
+├ rag.py
+├ cleaner.py
+├ embeddings.py
+│  ...
