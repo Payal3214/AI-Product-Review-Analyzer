30365da5a5a6f7b2f4d384fd045d08d1de752dac
Payal Priyadarshini <80968938+Payal3214@users.noreply.github.com>
2026-01-05 18:31:30 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,14 +1,18 @@
 import os
+import shutil
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-def load_vectordb():
-    persist_dir = "models/chroma_shoes"
-    embed = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+PERSIST_DIR = "/tmp/chroma_shoes"
 
-    if os.path.exists(persist_dir):
-        return Chroma(persist_directory=persist_dir, embedding_function=embed)
-    else:
-        os.makedirs(persist_dir, exist_ok=True)
-        return Chroma(persist_directory=persist_dir, embedding_function=embed)
+def load_vectordb():
+    # Force clean rebuild every time (Streamlit safe)
+    if os.path.exists(PERSIST_DIR):
+        shutil.rmtree(PERSIST_DIR)
+
+    embed = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return Chroma(persist_directory=PERSIST_DIR, embedding_function=embed)
+
+def fast_retriever(db):
+    return db.as_retriever(search_kwargs={"k":4})
 
