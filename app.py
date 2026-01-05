547f046cb847519133cc7df087a2c60a81fcb699
Payal Priyadarshini <80968938+Payal3214@users.noreply.github.com>
2026-01-05 16:54:52 +0530

    Update import path for rag module in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.getcwd())
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "")))
 
 import streamlit as st
-from src.rag import load_vectordb, fast_retriever
+from rag import load_vectordb, fast_retriever
 from transformers import pipeline
 from langchain_community.llms import HuggingFacePipeline
 from langchain.chains import RetrievalQA
